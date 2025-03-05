8dee7def0673a5a3b40fba5241d251d08c676571
Ahmed Torki <ahmedtorki@Ahmeds-MacBook-Air.local>
2025-03-05 09:05:27 -0800

    Updated Resume

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -71,7 +71,7 @@ const Home = () => {
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
                 <a
-                  href="https://drive.google.com/file/d/1awmsD4nChF9gqjFm3ATbE6EI78Zin0I2/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1n9BDSoANMKYkYHREGjhckBXCdMcqJDIB/view?usp=sharing"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
                   target="_blank"
                   rel="noopener noreferrer"
